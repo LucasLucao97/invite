18c8585c1d351110ed7e93200d0c83fc0f24ceea
Lucas Barbosa <lucas.barbosa12397@gmail.com>
2024-09-09 19:23:08 -0300

    test 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  output: 'export',
+  basePath: '/invite',
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
